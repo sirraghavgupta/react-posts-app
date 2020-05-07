acf512a24b0e2b0bc66704e8906a7bc6b3f76c3c
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-07 15:44:57 +0530

    post request learned

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 import './NewPost.css';
+import Axios from 'axios';
 
 class NewPost extends Component {
     state = {
         title: '',
-        content: '',
+        body: '',
         author: 'Max'
     }
 
@@ -15,6 +16,15 @@ class NewPost extends Component {
     
     componentDidUpdate = () => {
         console.log("updating the new post");
+    }
+
+    postPostHandler = () => {
+        console.log("posting data");
+        const data = {...this.state};
+        Axios.post("https://jsonplaceholder.typicode.com/posts", data)
+             .then( response => {
+                 console.log(response);
+             });
     }
 
     render () {
@@ -27,7 +37,7 @@ class NewPost extends Component {
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
                 
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.body} onChange={(event) => this.setState({body: event.target.value})} />
                
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
@@ -37,7 +47,7 @@ class NewPost extends Component {
 
                 </select>
                 
-                <button>Add Post</button>
+                <button onClick = { this.postPostHandler }>Add Post</button>
 
             </div>
         );
