c7f58868a6af0a8626911a6270c4f39e89e8404e
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-09 14:02:31 +0530

    basic setup - folder structure changed

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 // import Axios from 'axios';
@@ -64,18 +62,20 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className = "Blog">
+                <header>
+                    <nav>
+                        <ul>
+                        <li><a href = "/">Home</a></li>
+                        <li><a href = "/">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+
                 <section className="Posts">
                     {posts}
                 </section>
 
-                <section>
-                    <FullPost id = { this.state.selectedPostId }/>
-                </section>
-
-                <section>
-                    <NewPost />
-                </section>
             </div>
         );
     }
