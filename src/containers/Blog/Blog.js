7cdc33a18a1e71de3a66a92037d9f98483b38b59
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-09 19:04:55 +0530

    added the posts route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Posts from '../Blog/Posts/Posts';
 import './Blog.css';
+import { Route } from 'react-router-dom';
 
 
 class Blog extends Component {
@@ -18,8 +19,15 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
+            
+                {/* <Route path='/' exact render = { ()=><h1>home</h1> }/>
+                <Route path='/' render = { ()=><h1>home2</h1> }/> */}
 
-                <Posts/>
+                {/* <Route path='/' render = { ()=><Posts/> }/> */}
+
+                {/* we use the component prop and we need to pass a refernce 
+                of the functional or the class component. */}
+                <Route path='/' component = {Posts}/>
 
             </div>
         );
@@ -27,3 +35,22 @@ class Blog extends Component {
 }
 
 export default Blog;
+
+
+/**
+ * so, route is a self closing tag. 
+we can use as many route tags as we want and annywhere in the code, 
+may be nested inside a div or anything like that. 
+
+it requires the path prop, which route uses to match with the current 
+active path. it checks that it should only start with that mentioned in
+the path prop. else after that anything could be there in the active path.
+
+we need the exact prop if we want exact matching.
+
+inside render, we can render any JSX code or any component. route replaces
+itself with what render returns. 
+
+it checks each and every route component on the page and render all
+of the matching routes. 
+ */
